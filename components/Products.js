0afff5f6659b01782a8445d6d96877f9b1c3a58c
Blessing Krofegha <bkrofegha@gmail.com>
2020-06-08 14:41:10 +0200

    initial commit

--- components/Products.js
+++ components/Products.js
@@ -1,22 +1,16 @@
+import React from 'react'
+import Product from "./Product"
 
-import React, { Component } from 'react';
-import Product from './Product';
-
-class Products extends Component {
-
-  constructor(props) {
-    super(props);
-  }
- render() {
-      return (
-        <div>
-          <div className="container mt-4">
-            <div className="row">
-              {/* Map through the product props and return the item in the Product Component */}
-            </div>
-          </div>
+const Products = ({ products }) => {
+  return (
+    <div>
+      <div className="container mt-4">
+        <div className="row">
+          {/* Map through the product props and return the item in the Product Component */}
         </div>
-      )
- }
-};
-export default Products;
+      </div>
+    </div>
+  )
+}
+
+export default Products
